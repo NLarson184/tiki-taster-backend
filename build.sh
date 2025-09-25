92212ca1a8a90af594a760e69d38b64d93379a44
Nick Larson <nlarson184@gmail.com>
2025-09-24 21:01:31 -0700

    fix build file logic for manage.py location

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python tikitaster/manage.py collectstatic --no-input
 
-python manage.py migrate
+python tikitaster/manage.py migrate
